fbcd3d2a3555174aca3e131358480ee35008291b
Sharayu-P <sharayu.2506.patil@gmail.com>
2024-06-17 16:27:22 +0100

    bottom half and top half alignments

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,14 +4,16 @@ import Navbar from './JS/Navbar';
 import LNavbar from './JS/LNavbar';
 import Midbar from './JS/Midbar';
 import TopHalf from './JS/TopHalf';
+import BottomHalf from './JS/BottomHalf';
 
 function App() {
     return (
         <div className="App">
             <Navbar />
             <LNavbar />
-            <Midbar />
             <TopHalf />
+            <Midbar />
+            <BottomHalf />
         </div>
     );
 }
---
+++ Frontend/src/JS/BottomHalf.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../CSS/BottomHalf.css';
+
+function BottomHalf() {
+    return (
+        <div className="bottom-half">
+            <div className="card card1">
+                <h3>Card 1</h3>
+                <p>Details for Card 1</p>
+            </div>
+            <div className="card card2">
+                <h3>Card 2</h3>
+                <p>Details for Card 2</p>
+            </div>
+        </div>
+    );
+}
+
+export default BottomHalf;
