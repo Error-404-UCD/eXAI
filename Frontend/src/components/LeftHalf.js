1550f154f6a997cbcd8b3d55da4b0f7d38cb0d4e
// Sudhanshu <keysatmyfingertips@yahoo.com>
2024-07-19 17:47:34 +0100

    Added function to get prediction for counterfactuals
    - Can now paint over image
    - The image can now be exported to the lime and shap explainer
    - Removed paint file

--- Frontend/src/components/LeftHalf.js
+++ Frontend/src/components/LeftHalf.js
@@ -22,60 +22,59 @@ const LeftHalf = ({ fetchData }) => {
     };
 
     const handleDataFetch = () => {
-        // console.log(imageFile);
+        console.log(imageFile);
         fetchData(imageFile, selectedModel);
     };
 
     return (
-        <div className="bottom-half py-5 px-5 bg-slate-200">
-            <div className="flex flex-col items-center h-screen">
-                <div className="flex flex-none justify-start w-full py-4">
-                    <div className="upload-section mr-4">
-                        <input
-                            type="file"
-                            accept="image/*"
-                            onChange={handleImageChange}
-                            id="file-upload"
-                            hidden
-                        />
-                        <label
-                            htmlFor="file-upload"
-                            className="py-5 px-10 rounded-md text-sm font-semibold bg-white text-pink-700 hover:bg-pink-100 cursor-pointer shadow-md"
-                        >
-                            UPLOAD IMAGE
-                        </label>
-                    </div>
+      <div className="bottom-half py-5 px-5 bg-slate-200">
+        <div className="flex flex-col items-center h-screen">
+          <div className="flex flex-none justify-start w-full py-4">
+            <div className="upload-section mr-4">
+              <input
+                type="file"
+                accept="image/*"
+                onChange={handleImageChange}
+                id="file-upload"
+                hidden
+              />
+              <label
+                htmlFor="file-upload"
+                className="py-5 px-10 rounded-md text-sm font-semibold bg-white text-pink-700 hover:bg-pink-100 cursor-pointer shadow-md"
+              >
+                UPLOAD IMAGE
+              </label>
+            </div>
 
-                    <div className="predict-section">
-                        <input
-                            type="button"
-                            accept="image/*"
-                            onClick={handleDataFetch}
-                            id="predict-class"
-                            hidden
-                        />
-                        <label
-                            htmlFor="predict-class"
-                            className="py-5 px-10 rounded-md text-sm font-semibold bg-white text-pink-700 hover:bg-pink-100 cursor-pointer shadow-md"
-                        >
-                            PREDICT CLASS
-                        </label>
-                    </div>
-                </div>
+            <div className="predict-section">
+              <input
+                type="button"
+                accept="image/*"
+                onClick={handleDataFetch}
+                id="predict-class"
+                hidden
+              />
+              <label
+                htmlFor="predict-class"
+                className="py-5 px-10 rounded-md text-sm font-semibold bg-white text-pink-700 hover:bg-pink-100 cursor-pointer shadow-md"
+              >
+                PREDICT CLASS
+              </label>
+            </div>
+          </div>
 
-                <div className="mt-8 shadow-xl rounded-md px-5 py-5 bg-white relative">
-                    <Canvas
-                        imgUrl={image}
-                        width={400}
-                        height={400}
-                        alpha={100}
-                        isPaintable={true}
-                        
-                        
-                    />
-                </div>
+          <div className="mt-8 shadow-xl rounded-md px-5 py-5 bg-white relative">
+            <Canvas
+              imgUrl={image}
+              width={400}
+              height={400}
+              alpha={100}
+              isPaintable={true}
+              onExport={(url) => setImageFile(url)}
+            />
+          </div>
 
-                <div className="flex flex-row mr-4 text-lg py-5">
+          {/* <div className="flex flex-row mr-4 text-lg py-5">
                     <div className="px-5">DATASET</div>
                     <select
                         // onChange={console.log("Dataset changed")}
@@ -85,16 +84,16 @@ const LeftHalf = ({ fetchData }) => {
                         <option value="Astro">Astronomy</option>
                         <option value="BM">Bone Marrow</option>
                     </select>
-                </div>
-                <div className="flex flex-row mr-4 text-lg">
-                    <div className="px-5">ML Model</div>
-                    <select defaultValue={"M1"} onChange={handleModelChange}>
-                        <option value="M1">Super_CNN</option>
-                        <option value="M2">Tiny_CNN</option>
-                    </select>
-                </div>
-            </div>
+                </div> */}
+          <div className="flex flex-row mr-4 text-lg py-3">
+            <div className="px-5">ML Model</div>
+            <select defaultValue={"M1"} onChange={handleModelChange}>
+              <option value="M1">Super_CNN</option>
+              <option value="M2">Tiny_CNN</option>
+            </select>
+          </div>
         </div>
+      </div>
     );
 };
 
