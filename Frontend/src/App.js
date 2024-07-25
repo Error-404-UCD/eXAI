cd38aa41e2cb26a7c09a2573fd2bb907dc7cb8e4
Rujutas13 <ruju2000@gmail.com>
2024-07-25 15:19:31 +0100

    Landing Page added

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import Main from './components/Main'; 
 
-import Main from "./components/Main"
-
-function App() {
-    return (
-        <Main />
-    );
-}
+const App = () => {
+  return (
+    <Router>
+    <Routes>
+    <Route path="/" element={<LandingPage />} />
+    <Route path="/main" element={<Main />} />
+    </Routes>
+  </Router>
+  );
+};
 
 export default App;
