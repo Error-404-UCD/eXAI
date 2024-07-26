ce997b7bd42e8ad774e98b1d87d0ed2bd8440a4c
// Sudhanshu <keysatmyfingertips@yahoo.com>
2024-07-26 15:09:29 +0100

    Check for float when testing shap values

--- Backend/tests/test_shap_explainer.py
+++ Backend/tests/test_shap_explainer.py
@@ -32,7 +32,7 @@ class TestShapExplainer(unittest.TestCase):
         shap_values = ShapExplainer.get_explanation(self.blackbox, self.background, self.test_image)
 
         # Check the shap_values output
-        self.assertIsInstance(shap_values, np.ndarray)
+        self.assertIsInstance(shap_values[0][0][0][0][0], float)
         self.assertEqual(len(shap_values), 1)
         self.assertEqual(shap_values.shape, (self.test_image.shape[0], 150, 150, 3, 10))
 
